7a49690a0655a6b7797b62f4d156db14198ac6b4
Angelika01478 <angelika.solak@onet.eu>
2019-01-12 16:56:12 +0100

    first commit

---
+++ src/main/java/com/kodilla/kodillacourselearning/KodillaCourseLearningApplication.java
@@ -0,0 +1,14 @@
+package com.kodilla.kodillacourselearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaCourseLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaCourseLearningApplication.class, args);
+    }
+
+}
+
