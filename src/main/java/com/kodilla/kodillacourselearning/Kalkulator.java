d3f797a79dcad9fff8d59228733d10383d320555
Angelika01478 <angelika.solak@onet.eu>
2019-01-12 17:09:01 +0100

    first commit

--- src/main/java/com/kodilla/kodillacourselearning/Kalkulator.java
+++ src/main/java/com/kodilla/kodillacourselearning/Kalkulator.java
@@ -1,4 +1,21 @@
 package com.kodilla.kodillacourselearning;
 
 public class Kalkulator {
+
+    double add (double a, double b){
+        return a+b;
+    }
+
+    double subtract (double a, double b){
+        return a-b;
+    }
+
+    public static void main(String[] args) {
+        Kalkulator kalkulator = new Kalkulator();
+        double firstValue = kalkulator.add(15.4,8);
+        double secondValue= kalkulator.subtract(25.785, 18.11);
+
+        System.out.println(firstValue);
+        System.out.println(secondValue);
+    }
 }
