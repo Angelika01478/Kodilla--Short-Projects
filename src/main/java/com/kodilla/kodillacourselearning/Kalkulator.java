69e34340551257bd6cec6e6292506ff8cee82a40
Angelika01478 <angelika.solak@onet.eu>
2019-01-12 17:14:02 +0100

    calculator

--- src/main/java/com/kodilla/kodillacourselearning/Kalkulator.java
+++ src/main/java/com/kodilla/kodillacourselearning/Kalkulator.java
@@ -6,6 +6,7 @@ public class Kalkulator {
         return a+b;
     }
 
+
     double subtract (double a, double b){
         return a-b;
     }
@@ -15,7 +16,7 @@ public class Kalkulator {
         double firstValue = kalkulator.add(15.4,8);
         double secondValue= kalkulator.subtract(25.785, 18.11);
 
-        System.out.println(firstValue);
-        System.out.println(secondValue);
+        System.out.println("the result is equal: " +firstValue);
+        System.out.println("the result is equal: " +secondValue);
     }
 }
